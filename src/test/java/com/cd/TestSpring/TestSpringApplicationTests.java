44b23a55233c6316ba53b351f79188afdd21fa0a
Charith-Dhananjaya <114970084+Charith-Dhananjaya@users.noreply.github.com>
2025-05-07 21:01:33 +0530

    add initials

---
+++ src/test/java/com/cd/TestSpring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cd.TestSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
