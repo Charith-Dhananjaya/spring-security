283378c53fb568115f6011bc522e20eb1fc22dea
Charith-Dhananjaya <114970084+Charith-Dhananjaya@users.noreply.github.com>
2025-05-10 19:57:59 +0530

    use rest Apis

--- src/main/java/com/cd/TestSpring/controller/MyController.java
+++ src/main/java/com/cd/TestSpring/controller/MyController.java
@@ -14,15 +14,29 @@ public class MyController {
 
     private Map<Long,checkEntry> checkEntries =  new HashMap<>();
 
-    @GetMapping("/get")
+    @GetMapping("get")
     public List<checkEntry> getAll(){
         return  new ArrayList<>(checkEntries.values());
     }
 
-    @PostMapping("/post")
+    @PostMapping("post")
     public checkEntry createEntry(@RequestBody checkEntry myEntry){
             checkEntries.put(myEntry.getId(), myEntry);
             return myEntry;
     }
 
+    @GetMapping("id/{myId}")
+    public checkEntry getEntryById(@PathVariable Long myId){
+        return  checkEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+        public checkEntry deleteEntryById(@PathVariable Long myId){
+            return checkEntries.remove(myId);
+        }
+
+    @PutMapping("id/{id}")
+    public  checkEntry updateEntryById(@PathVariable Long id, @RequestBody checkEntry myEntry){
+        return  checkEntries.put(id, myEntry);
+    }
 }
