069c362f9ac62f4e88f4a251ba4b0541eda48257
Charith-Dhananjaya <114970084+Charith-Dhananjaya@users.noreply.github.com>
2025-05-12 00:01:22 +0530

    update controller layer

---
+++ src/main/java/com/cd/TestSpring/controller/StudentController.java
@@ -0,0 +1,42 @@
+package com.cd.TestSpring.controller;
+import com.cd.TestSpring.entity.StudentEntry;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+@RestController
+@RequestMapping("/_rest")
+public class StudentController {
+
+    private Map<String, StudentEntry> studentEntries =  new HashMap<>();
+
+    @GetMapping("get")
+    public List<StudentEntry> getAll(){
+        return  new ArrayList<>(studentEntries.values());
+    }
+
+    @PostMapping("post")
+    public StudentEntry createEntry(@RequestBody StudentEntry studentEntry){
+        studentEntries.put(studentEntry.getId(), studentEntry);
+            return studentEntry;
+    }
+
+    @GetMapping("id/{myId}")
+    public StudentEntry getEntryById(@PathVariable Long myId){
+        return  studentEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+        public StudentEntry deleteEntryById(@PathVariable Long myId){
+            return studentEntries.remove(myId);
+        }
+
+    @PutMapping("id/{id}")
+    public StudentEntry updateEntryById(@PathVariable String id, @RequestBody StudentEntry myEntry){
+        return  studentEntries.put(id, myEntry);
+    }
+}
