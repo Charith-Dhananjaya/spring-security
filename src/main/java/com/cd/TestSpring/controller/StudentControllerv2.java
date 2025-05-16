bff497dc823bee64e22ef1a44b6bac39093f4c98
Charith-Dhananjaya <114970084+Charith-Dhananjaya@users.noreply.github.com>
2025-05-16 22:49:50 +0530

    update controller

--- src/main/java/com/cd/TestSpring/controller/StudentControllerv2.java
+++ src/main/java/com/cd/TestSpring/controller/StudentControllerv2.java
@@ -18,30 +18,38 @@ public class StudentControllerv2 {
 
 
     @GetMapping("get")
-    public List<StudentEntry> getAll(){
-        return null;
+    public List<StudentEntry> getAllData(){
+        return studentService.getAll();
     }
 
     @PostMapping
-    public StudentEntry createEntry(@RequestBody StudentEntry studentEntry){
-            studentEntry.setDate(LocalDateTime.now());
-            studentService.saveEntry(studentEntry);
-            return studentEntry;
+    public StudentEntry createEntry(@RequestBody StudentEntry myEntry) {
+        myEntry.setDate(LocalDateTime.now());
+        studentService.saveEntry(myEntry);
+        return myEntry;
     }
 
     @GetMapping("id/{myId}")
-    public StudentEntry getEntryById(@PathVariable ObjectId myId){
+    public StudentEntry getEntryById(@PathVariable ObjectId myId) {
         return studentService.findById(myId).orElse(null);
     }
 
     @DeleteMapping("id/{myId}")
-    public boolean deleteEntryById(@PathVariable ObjectId myId){
+    public boolean deleteEntryById(@PathVariable ObjectId myId) {
         studentService.deleteById(myId);
         return true;
     }
 
     @PutMapping("id/{id}")
-    public StudentEntry updateEntryById(@PathVariable ObjectId id, @RequestBody StudentEntry studentEntry){
-        return null;
+    public StudentEntry updateEntryById(@PathVariable ObjectId id, @RequestBody StudentEntry newEntry){
+        StudentEntry oldEntry = studentService.findById(id).orElse(null);
+        if(oldEntry != null){
+            oldEntry.setTitle(newEntry.getTitle() != null && newEntry.getTitle().equals("") ? newEntry.getTitle() : oldEntry.getTitle());
+            oldEntry.setName(newEntry.getName() != null && newEntry.getName().equals("") ? newEntry.getName() : oldEntry.getName());
+
+        }
+
+        studentService.saveEntry(oldEntry);
+        return oldEntry;
     }
 }
