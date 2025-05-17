d18bfedf0d1c0fa7c1ba1fd2fd344288028638a8
Charith-Dhananjaya <114970084+Charith-Dhananjaya@users.noreply.github.com>
2025-05-17 22:21:25 +0530

    add response codes to APIs

--- src/main/java/com/cd/TestSpring/controller/StudentControllerv2.java
+++ src/main/java/com/cd/TestSpring/controller/StudentControllerv2.java
@@ -21,42 +21,49 @@ public class StudentControllerv2 {
 
 
     @GetMapping("get")
-    public List<StudentEntry> getAllData(){
-        return studentService.getAll();
+    public ResponseEntity<?> getAllData(){
+        List<StudentEntry> all = studentService.getAll();
+        if(all != null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public StudentEntry createEntry(@RequestBody StudentEntry myEntry) {
-        myEntry.setDate(LocalDateTime.now());
-        studentService.saveEntry(myEntry);
-        return myEntry;
+    public ResponseEntity<StudentEntry> createEntry(@RequestBody StudentEntry myEntry) {
+        try{
+            myEntry.setDate(LocalDateTime.now());
+            studentService.saveEntry(myEntry);
+            return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("id/{myId}")
     public ResponseEntity<StudentEntry> getEntryById(@PathVariable ObjectId myId) {
         Optional<StudentEntry> studentEntry = studentService.findById(myId);
         if(studentEntry.isPresent()){
-            return  new ResponseEntity<>(studentEntry.get(), HttpStatus.OK);
+            return new ResponseEntity<>(studentEntry.get(), HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("id/{myId}")
-    public boolean deleteEntryById(@PathVariable ObjectId myId) {
+    public ResponseEntity<?> deleteEntryById(@PathVariable ObjectId myId) {
         studentService.deleteById(myId);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("id/{id}")
-    public StudentEntry updateEntryById(@PathVariable ObjectId id, @RequestBody StudentEntry newEntry){
+    public ResponseEntity<?> updateEntryById(@PathVariable ObjectId id, @RequestBody StudentEntry newEntry){
         StudentEntry oldEntry = studentService.findById(id).orElse(null);
         if(oldEntry != null){
             oldEntry.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : oldEntry.getTitle());
             oldEntry.setName(newEntry.getName() != null && !newEntry.getName().equals("") ? newEntry.getName() : oldEntry.getName());
-
+            studentService.saveEntry(oldEntry);
+            return new ResponseEntity<>(oldEntry, HttpStatus.OK);
         }
-
-        studentService.saveEntry(oldEntry);
-        return oldEntry;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
